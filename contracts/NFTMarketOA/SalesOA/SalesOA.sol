cd7bbfa795faefe5a84f5fb7915dbfc05bc0bbce
"Diego" <"Diegocortes079@gmail.com">
2022-05-09 16:59:11 -0500

    Add contract to put items in sale and buy it

---
+++ contracts/NFTMarketOA/SalesOA/SalesOA.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../StorageOA/IStorageOA.sol";
+
+contract SalesOA is ReentrancyGuard {
+
+    // Mapping of approved address to send contract's nfts
+    mapping(address => bool) private _approvals;
+
+    address payable owner;
+    address address_storage;
+    uint256 listingPrice;
+
+    constructor(uint256 _listingPrice, address _address_storage) payable {
+        owner = payable(msg.sender);
+        listingPrice = _listingPrice;
+        address_storage = _address_storage;
+
+    }
+
+
+
+    function myallowance(address currency) view public returns (uint){
+        IERC20 erc20 = IERC20(currency);
+        return erc20.allowance(msg.sender,address(this));
+    }
+
+    /* Returns the listing price of the contract */
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+
+
+    /* Transfers ownership of the item, as well as funds between parties */
+    function createMarketSale(uint256 itemId, address buyer) public payable nonReentrant {
+        IStorageOA iStorage = IStorageOA(address_storage);
+        IStorageOA.StorageItem memory item = iStorage.getItem(itemId);
+        require(item.onSale, "This Item is not on sale.");
+        require(item.owner != buyer, "You are the owner of this nft");
+        require(item.currency != address(0) || msg.value == item.price, "The value sent must be equals to nft's price");
+
+        uint256 price = item.price;
+
+        if (item.currency != address(0)) {
+            IERC20 erc20 = IERC20(item.currency);
+            require(
+                erc20.transferFrom(buyer, address(this), price) &&
+                erc20.transfer(item.owner, (price - (price * listingPrice / 100))) &&
+                erc20.transfer(owner, (price * listingPrice / 100))
+            , "Transaction failed at pay item");
+        } else {
+            payable(item.owner).transfer((price - (price * listingPrice / 100)));
+            payable(owner).transfer((price * listingPrice / 100));
+        }
+
+        iStorage.transferItem(itemId, buyer);
+    }
+
+    /* Change listing price in hundredths*/
+    function setListingPrice (uint percent) public {
+        require(msg.sender == owner, "You are not allowed to execute this function");
+        listingPrice = percent;
+    }
+
+    /* Put on sale */
+    function activateSale(uint256 itemId, uint256 price, address currency, address seller) public {
+        IStorageOA iStorage = IStorageOA(address_storage);
+        IStorageOA.StorageItem memory item = iStorage.getItem(itemId);
+        require(seller == item.owner, "You are not owner of this nft");
+        require(!item.onSale, "This item is on sale already");
+        require(!item.onAuction, "This item is currently on auction");        
+        IERC721(item.nftContract).transferFrom(item.owner, address_storage, item.tokenId);
+        iStorage.setItem(itemId, payable(seller), price, false, true, currency, true, address_storage);
+    }
+
+    /* Remove from sale */
+    function deactivateSale(uint256 itemId, address seller) public {
+        IStorageOA iStorage = IStorageOA(address_storage);
+        IStorageOA.StorageItem memory item = iStorage.getItem(itemId);
+        require(seller == item.owner, "You are not owner of this nft");
+        iStorage.transferItem(itemId, seller);
+    }
+}
