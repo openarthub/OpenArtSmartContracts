1a9bdb1300c9d2b9de8def49daa7eafb4e80f602
"Diego" <"Diegocortes079@gmail.com">
2022-05-10 09:02:42 -0500

    Add modifier validations

--- contracts/NFTMarketOA/SalesOA/SalesOA.sol
+++ contracts/NFTMarketOA/SalesOA/SalesOA.sol
@@ -16,6 +16,18 @@ contract SalesOA is ReentrancyGuard {
     address address_storage;
     uint256 listingPrice;
 
+    /* Modifier to allow only owner to execute a function */
+    modifier onlyOwner {
+        require(msg.sender == owner, "You are not allowed to execute this function");
+        _;
+    }
+
+    /* Modifier to allow only address stored in _approvals */
+    modifier onlyApprovals {
+        require(_approvals[msg.sender], "You are not allowed to execute this method");
+        _;
+    }
+
     constructor(uint256 _listingPrice, address _address_storage) payable {
         owner = payable(msg.sender);
         listingPrice = _listingPrice;
@@ -23,8 +35,7 @@ contract SalesOA is ReentrancyGuard {
 
     }
 
-
-
+    /* Return allowance in a specific ERC20 token */
     function myallowance(address currency) view public returns (uint){
         IERC20 erc20 = IERC20(currency);
         return erc20.allowance(msg.sender,address(this));
@@ -35,10 +46,8 @@ contract SalesOA is ReentrancyGuard {
         return listingPrice;
     }
 
-
-
     /* Transfers ownership of the item, as well as funds between parties */
-    function createMarketSale(uint256 itemId, address buyer) public payable nonReentrant {
+    function createMarketSale(uint256 itemId, address buyer) onlyApprovals public payable nonReentrant {
         IStorageOA iStorage = IStorageOA(address_storage);
         IStorageOA.StorageItem memory item = iStorage.getItem(itemId);
         require(item.onSale, "This Item is not on sale.");
@@ -63,13 +72,12 @@ contract SalesOA is ReentrancyGuard {
     }
 
     /* Change listing price in hundredths*/
-    function setListingPrice (uint percent) public {
-        require(msg.sender == owner, "You are not allowed to execute this function");
+    function setListingPrice (uint percent) onlyOwner public {
         listingPrice = percent;
     }
 
     /* Put on sale */
-    function activateSale(uint256 itemId, uint256 price, address currency, address seller) public {
+    function activateSale(uint256 itemId, uint256 price, address currency, address seller) onlyApprovals public {
         IStorageOA iStorage = IStorageOA(address_storage);
         IStorageOA.StorageItem memory item = iStorage.getItem(itemId);
         require(seller == item.owner, "You are not owner of this nft");
@@ -80,10 +88,15 @@ contract SalesOA is ReentrancyGuard {
     }
 
     /* Remove from sale */
-    function deactivateSale(uint256 itemId, address seller) public {
+    function deactivateSale(uint256 itemId, address seller) onlyApprovals public {
         IStorageOA iStorage = IStorageOA(address_storage);
         IStorageOA.StorageItem memory item = iStorage.getItem(itemId);
         require(seller == item.owner, "You are not owner of this nft");
         iStorage.transferItem(itemId, seller);
+    }
+
+    /* Set storage address */
+    function setStorageAddress (address _address_storage) onlyOwner public {
+        address_storage = _address_storage;
     }
 }
