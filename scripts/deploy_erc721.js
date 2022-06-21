3eeca5ddf283c1f2a421aaa4fea5578ad5f24d9a
Diego <Diegocortes079@gmail.com>
2022-06-21 10:57:13 -0500

    Add addresses contracts as parameters

--- scripts/deploy_erc721.js
+++ scripts/deploy_erc721.js
@@ -7,7 +7,8 @@ const main = async () => {
   /* *** ERC721OA *** */
   const ERC721OA = await hre.ethers.getContractFactory('NFTEth')
   const erc721OA = await ERC721OA.deploy(
-    '0x8486770dF0a8FFA81d9380e0f2D9a4bbc554e8C0', // Address of market contract
+    '0x0e486ACF8B867AA79361E9346C4918cf7B5cd7CD', // Address of sales contract
+    '0xE8b0f5B2036FaaCe506f20bb9829F88aE60D2d22', // Address of auctions contract
   )
 
   await erc721OA.deployed()
