ab5adc7edb2c31faaf056dfe380b9eefc435b914
Diego <Diegocortes079@gmail.com>
2022-06-21 10:52:10 -0500

    Create storage lite interface

---
+++ contracts/NFTMarketOA/StorageOA/IStorageOATrusted.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.4;
+
+interface IStorageOA {
+    function trustedCreateItem(
+    address nftContract,
+    uint256 tokenId,
+    bool isActive,
+    address ownerItem,
+    bool onSale,
+    bool onAuction,
+    uint256 endTime,
+    address currency,
+    uint256 price,
+    address highestBidder,
+    uint256 highestBid
+  ) external;
+}
