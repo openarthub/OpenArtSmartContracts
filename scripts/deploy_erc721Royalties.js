16bc2ef104d29a0b39578ca12b15c8108007cf42
Diego <Diegocortes079@gmail.com>
2022-09-14 13:19:00 -0500

    Adapt script to new contract

--- scripts/deploy_erc721Royalties.js
+++ scripts/deploy_erc721Royalties.js
@@ -1,21 +1,28 @@
 // Right click on the script name and hit "Run" to execute
-const hre = require("hardhat")
+const hre = require('hardhat');
 
 const main = async () => {
-  console.log('Running deployWithEthers script...')
+  console.log('Running deployWithEthers script...');
 
   /* *** ERC721OA *** */
-  const ERC721OA = await hre.ethers.getContractFactory("ERC721Royalties")
-  const erc721OA = await ERC721OA.deploy("RoyaltiesCollection", "RC");
+  const ERC721OA = await hre.ethers.getContractFactory('FOLEARTMX');
+  const erc721OA = await ERC721OA.deploy(
+    'Examp', // name
+    'FAMX', // symbol
+    '0x794Fed8D9bEe97a7e352e4C308Cec5632b741104', // Address owner
+    1, // max supply
+  );
 
-  await erc721OA.deployed()
-  const erc721OAAddress = erc721OA.address
-  console.log(`ERC721OA smart contract deployed at ${erc721OAAddress}`)
-}
+  await erc721OA.deployed();
+  const erc721OAAddress = erc721OA.address;
+  console.log(`ERC721OA smart contract deployed at ${erc721OAAddress}`);
+  await erc721OA.setApproval('0x63594Ca39d89dfD0EB5E310AC20353538088c5d9', true);
+  console.log('setted');
+};
 
-main ()
+main()
   .then(() => process.exit(0))
-  .catch(error => {
-    console.log(error)
-    process.exit(1)
-  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
